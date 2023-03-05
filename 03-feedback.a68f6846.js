70adfa741379d3f950901601f1aba29c714de9cf
NadiaZaichenko <114259833+NadiaZaichenko@users.noreply.github.com>
2023-03-05 16:59:06 +0000

    Deploying to gh-pages from @ NadiaZaichenko/goit-js-hw-08@48f1c9bb3a7cf384a44868930590ed451a4c92d1 🚀

---
+++ 03-feedback.a68f6846.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".feedback-form");var e=document.querySelector("input");document.querySelector("textarea");e.addEventListener("input",(function(t){console.log(e.value)}))}();
+//# sourceMappingURL=03-feedback.a68f6846.js.map
---
+++ 03-feedback.c9e21c7a.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form");const e=document.querySelector("input");document.querySelector("textarea");e.addEventListener("input",(function(t){console.log(e.value)}));
+//# sourceMappingURL=03-feedback.c9e21c7a.js.map
