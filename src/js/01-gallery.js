7c8da6882569b61e266c378ca5852b83e1925db6
NadiaZaichenko <nadyazaichenko884@gmail.com>
2023-03-05 18:15:19 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,31 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+ 
+
+const divGalleryRef = document.querySelector('div.gallery')
+const markup = createMarkup(galleryItems);
+
+function createMarkup(items) {
+    return items.map(({preview, original, description}) => {
+  return `
+  <a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"/>
+</a>
+`}).join('');
+};
+
+divGalleryRef.insertAdjacentHTML('beforeend', markup) ;
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const player = new Player('vimeo-player');
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = function(data){
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
+  
