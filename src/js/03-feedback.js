06eee907d26c251eecd8146158bb4502f96fcc07
NadiaZaichenko <nadyazaichenko884@gmail.com>
2023-03-06 11:12:32 +0200

    Homework

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,42 @@
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input');
 const messege = document.querySelector('textarea');
 
+const STORAGE_KEY = "feedback-form-state"
 
+const formData = {};
 
-const formInput = function(evn) {
-    console.log(email.value)
+form.addEventListener('input', throttle(onForm, 500));
+form.addEventListener('submit', onFormSubmit);
+
+populateForm ();
+
+function onForm (evt){
+ formData[evt.target.name] = evt.target.value;  
+ localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+ 
 }
 
-email.addEventListener('input', formInput);
-const STORAGE_KEY = "feedback-form-state"
+function onFormSubmit(evt) {
+ evt.preventDefault();
+ form[{email, messege}] = evt.target.elements;
+ if (email.value === "" || messege.value === "") {
+   return alert('Заповніть будь ласка всі поля');
+ }
+console.log({email: email.value, messege: messege.value});
+ console.log('Відправляємо форму');
+ evt.target.reset();
+localStorage.removeItem(STORAGE_KEY);
+};
+
+function populateForm (evt) {
+const contentForm = localStorage.getItem(STORAGE_KEY);
+    if(contentForm){
+        const parseContentForm = JSON.parse(contentForm)
+        // console.log(parseContentForm);
+        messege.value = parseContentForm.message;
+        email.value = parseContentForm.email;
+};
+};
